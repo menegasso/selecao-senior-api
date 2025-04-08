803877a5b6385fe499fde71334b9c7e6afcfd94a
Andre Menegasso <meneghasso@gmail.com>
2025-04-08 15:20:54 -0300

    Base structure

---
+++ src/main/java/com/selecao/senior/api/SelecaoSeniorApiApplication.java
@@ -0,0 +1,13 @@
+package com.selecao.senior.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoSeniorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaoSeniorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selecao/senior/api/SelecaoSeniorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selecao.senior.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaoSeniorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
